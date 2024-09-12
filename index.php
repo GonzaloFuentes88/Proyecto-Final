e6480a7f5e4bf679f5d8a1e86452881f0978d660
gonzalofuentes1 <fuentesgonzalo88@gmail.com>
2024-09-11 23:11:21 -0300

    Primer commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+// require_once '../config/Database.php';
+
+// $database = new Database();
+// $conn = $database->getConnection();
+
+// if ($conn) {
+//     echo "Conexión exitosa a la base de datos";
+// } else {
+//     echo "Error al conectar a la base de datos";
+// }
+
+echo "Hola mundo";
