20abd8358bc6f896f86acc7e7ee7d7dd9afdb266
gonzalofuentes1 <fuentesgonzalo88@gmail.com>
2024-11-20 19:51:42 -0300

    Cambios en admin-evento y miseventos

--- frontend/scripts/admin/eventos.js
+++ frontend/scripts/admin/eventos.js
@@ -1,8 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('.container-evento').forEach(item => {
-        item.addEventListener('click', () => {
-            const details = item.querySelector('.evento-details');
-            details.classList.toggle('d-none'); 
+    const toggleButtons = document.querySelectorAll('.toggleButton');
+
+    toggleButtons.forEach(button => {
+        button.addEventListener('click', function () {
+            const eventoDetails = button.parentElement.nextElementSibling;
+
+            eventoDetails.classList.toggle('d-none');
+
+            const arrowIcon = button.querySelector('.arrowIcon');
+            arrowIcon.classList.toggle('fa-chevron-left');
+            arrowIcon.classList.toggle('fa-chevron-down');
+
+            if (!eventoDetails.classList.contains('open')) {
+                eventoDetails.style.height = eventoDetails.scrollHeight + 'px';
+                eventoDetails.classList.add('open');
+
+                setTimeout(() => {
+                    eventoDetails.style.height = 'auto';
+                }, 500);
+            } else {
+                eventoDetails.style.height = eventoDetails.scrollHeight + 'px';
+                setTimeout(() => {
+                    eventoDetails.style.height = '0';
+                }, 10);
+                eventoDetails.classList.remove('open');
+            };
         });
     });
 });
+
